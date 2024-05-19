563ee58211dfb6cb93fa909cdfc2c8f1b024c1a6
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2024-05-19 16:27:54 +0530

    made thr call polyfill

---
+++ polyfills/call.js
@@ -0,0 +1,20 @@
+function getName(adderess= " " , age = 0){
+    console.log(this.firstName + " " + this.lastName + ' of age ' + age + ' lives in ' + adderess) 
+}
+
+let obj = {
+    firstName : "Abhijeet",
+    lastName : "Mukherjee"
+}
+
+getName.call(obj, "Kadru", 27)
+
+Function.prototype.myCall = function(context, ...args){
+    console.log(this, "this", context)
+    if(typeof this !== "function"){
+        throw new Error(`${this} is not a function`)
+    }
+    context.fn = this;
+    context.fn(...args)
+}
+getName.myCall(obj);
