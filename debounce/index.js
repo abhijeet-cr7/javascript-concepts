9eaadd1e9aef3d25b2f64b352fbf6d043934c6bd
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2024-05-20 01:09:03 +0530

    added throttle code in javascript

--- debounce/index.js
+++ debounce/index.js
@@ -1,18 +1,32 @@
 const buttonOne = document.querySelector(".header");
+const buttonTwo = document.querySelector(".throttle");
 const pressedPara = document.querySelector(".pressed");
 const triggeredPara = document.querySelector(".triggered");
+const throttledPara = document.querySelector(".throttlepressed");
+const throttledTriggered = document.querySelector(".throttletriggered");
 
 let pressedCount = 0;
 let triggeredCount = 0;
+let throttleCount = 0;
+let triggeredThrottleCount = 0;
 
  const debouncedPolyfill = (callback, delay) => {
     let timeout;
     return function(...args){
-        console.log(args, "args", timeout);
         if(timeout) clearTimeout(timeout);
         timeout = setTimeout(() => {
             callback(...args)
         },delay)
+    }
+}
+
+const throttlePolyfill = (callback, delay) => {
+    let then = 0;
+    return function(...args){
+        let now = new Date().getTime();
+        if((now -then)<delay) return;
+        then = now;
+        callback(...args);
     }
 }
 
@@ -21,9 +35,20 @@ let debounce = debouncedPolyfill(() => {
     triggeredPara.innerHTML = `<p>Triggerd ${triggeredCount}</p>`;
 }, 500)
 
+let throttle = throttlePolyfill(() => {
+    ++triggeredThrottleCount,
+    throttledTriggered.innerHTML = `<p> Throttle Triggered ${triggeredThrottleCount}`;
+},1000)
+
 buttonOne.addEventListener("click" , () => {
     ++pressedCount;
     pressedPara.innerHTML = `<p>Count ${pressedCount}</p>`; 
     debounce("hello") 
+})
+
+buttonTwo.addEventListener("click", () => {
+    ++throttleCount;
+    throttledPara.innerHTML = `<p>Count ${throttleCount}<p>`
+    throttle("pubali")
 })
 
