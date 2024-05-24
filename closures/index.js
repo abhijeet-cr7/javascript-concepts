afac1e36cec279bb7dc92d4a5f70011f18564474
abhijeet-cr7 <abhijeetdbz7@gmail.com>
2024-05-24 13:29:37 +0530

    changes made for closures

--- closures/index.js
+++ closures/index.js
@@ -13,3 +13,45 @@ function find(index){
 console.time("6");
 console.log(find(6)); 
 console.timeEnd("6");
+
+for(var i=0; i<3; i++){
+    function Babu(i){
+        setTimeout(() => {
+            console.log(i, "i")
+        }, 1000)
+    }
+    Babu(i);
+}
+
+var Module = (function(){
+    function privateMethod(){
+        console.log('private method');
+    }
+    return {publicMethod : function(){
+        privateMethod();
+        console.log("public method");
+    }}
+})();
+
+Module.publicMethod();
+
+function likeVideo(){
+    let called = 0;
+    return function subscribed(){
+        if(called === 0){
+            console.log("subscribed once");
+            called++;
+        }
+        else{
+            console.log("already subscribed");
+        }
+    }
+}
+
+const subscribed = likeVideo();
+subscribed();
+subscribed();
+subscribed();
+subscribed();
+subscribed();
+subscribed();
